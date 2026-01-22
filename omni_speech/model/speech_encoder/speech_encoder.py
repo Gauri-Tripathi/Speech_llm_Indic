884a2574e9354b597ff3a682992cff02d91345fa
Gauri-Tripathi <gauritr19@gmail.com>
2026-01-22 12:00:00 +0530

    update omni_speech/model/speech_encoder/speech_encoder.py

--- omni_speech/model/speech_encoder/speech_encoder.py
+++ omni_speech/model/speech_encoder/speech_encoder.py
@@ -10,17 +10,17 @@ import torch.nn.functional as F
 class WhisperWrappedEncoder:
     
     @classmethod
-    def load(cls, model_config):
+    def load(cls, model_config, freeze_encoder=True):
         speech_encoder_path = model_config.speech_encoder
         
         # Check if it's a HuggingFace model directory
         if os.path.isdir(speech_encoder_path) and os.path.exists(os.path.join(speech_encoder_path, "config.json")):
-            return cls._load_hf_whisper(speech_encoder_path)
+            return cls._load_hf_whisper(speech_encoder_path, freeze_encoder)
         else:
-            return cls._load_openai_whisper(speech_encoder_path)
+            return cls._load_openai_whisper(speech_encoder_path, freeze_encoder)
     
     @classmethod
-    def _load_hf_whisper(cls, model_path):
+    def _load_hf_whisper(cls, model_path, freeze_encoder=True):
         """Load Whisper encoder from HuggingFace format."""
         from transformers import WhisperModel
         
@@ -28,14 +28,20 @@ class WhisperWrappedEncoder:
         whisper_model = WhisperModel.from_pretrained(model_path)
         encoder = whisper_model.encoder
         
-        # Freeze encoder weights by default
-        for param in encoder.parameters():
-            param.requires_grad = False
+        # Freeze encoder weights based on parameter
+        if freeze_encoder:
+            print("Freezing Whisper encoder weights")
+            for param in encoder.parameters():
+                param.requires_grad = False
+        else:
+            print("Keeping Whisper encoder trainable")
+            for param in encoder.parameters():
+                param.requires_grad = True
             
         return encoder
     
     @classmethod
-    def _load_openai_whisper(cls, model_name_or_path):
+    def _load_openai_whisper(cls, model_name_or_path, freeze_encoder=True):
         """Load Whisper encoder from OpenAI format."""
         def replace_layer_norm(module):
             from whisper.model import LayerNorm
@@ -52,4 +58,15 @@ class WhisperWrappedEncoder:
         print(f"Loading Whisper encoder from OpenAI format: {model_name_or_path}")
         encoder = whisper.load_model(name=model_name_or_path, device='cpu').encoder
         replace_layer_norm(encoder)
+        
+        # Freeze encoder weights based on parameter
+        if freeze_encoder:
+            print("Freezing Whisper encoder weights")
+            for param in encoder.parameters():
+                param.requires_grad = False
+        else:
+            print("Keeping Whisper encoder trainable")
+            for param in encoder.parameters():
+                param.requires_grad = True
+                
         return encoder
